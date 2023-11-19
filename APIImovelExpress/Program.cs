685b6283207ca0d5d222ca72608f66c7ae13c9a0
Percy <percy@railway.app>
2023-11-19 02:34:05 +0000

    Initial Commit

---
+++ APIImovelExpress/Program.cs
@@ -0,0 +1,27 @@
+using Infra.IoC;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+builder.Services.AddInfraInjection(builder.Configuration);
+
+builder.Services.AddCors(opt => opt.AddPolicy("AllowRequest", builder => builder.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader()));
+
+var app = builder.Build();
+
+
+app.UseSwagger();
+app.UseSwaggerUI();
+
+app.UseHttpsRedirection();
+app.UseRouting();
+app.UseCors(opt => opt.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader());
+
+app.UseAuthentication();
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
